f57fc986e8d52b3777d2a776db2c39d48fe1f4a4
LiranMamanA <139689926+LiranMamanA@users.noreply.github.com>
2023-07-16 16:21:35 +0300

    Add files via upload

---
+++ text_generation.py
@@ -0,0 +1,27 @@
+import nltk
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+
+def analyze_sentiment(text):
+    sid = SentimentIntensityAnalyzer()
+    sentiment_scores = sid.polarity_scores(text)
+
+    if sentiment_scores['compound'] >= 0.05:
+        return "Positive"
+    elif sentiment_scores['compound'] <= -0.05:
+        return "Negative"
+    else:
+        return "Neutral"
+
+
+def main():
+    nltk.download('vader_lexicon')
+    print("Sentiment Analysis Tool")
+    print("-----------------------")
+    text = input("Enter text to analyze sentiment: ")
+    sentiment = analyze_sentiment(text)
+    print("Sentiment:", sentiment)
+
+
+if __name__ == "__main__":
+    main()
